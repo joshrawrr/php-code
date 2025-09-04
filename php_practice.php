b8a7294cfac6d90ae8a2a4f61b4cb236c9e97536
Allan Joshua C. Manalo <163095394+joshrawrr@users.noreply.github.com>
2025-09-04 20:04:44 +0800

    Add files via upload

---
+++ php_practice.php
@@ -0,0 +1,227 @@
+
+<!--Hello!
+<h1>Hello!</h1>
+<style>
+    h1 {
+        color: yellow;
+
+    }
+</style>
+
+<script>
+    alert("Hello!")
+</script> -->
+<!--<?php
+
+    echo "<b>Hello World!</b>";
+?>-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello this is Me!</h1>
+    <?php 
+        //ECHo "Hello!";
+
+       // $color = "Red";
+       // $Color = "Blue";
+       // echo $Color:
+       // echo $color;
+
+       // $_ =5;
+       // echo $_;
+
+       // var_dump($color);
+       // var_dump($_);
+       // var_dump(3.1);
+       // var_dump(true);
+       // var_dump(false);
+       // var_dump([1,2]);
+
+       // $x = 5; //global
+
+       //function test(){
+           // $x = 10; //local
+            //static $x = 10;
+           // echo $x;
+           // $x++;
+           // echo "<br>";
+       // }
+
+       // test();
+       // test();
+       // test();
+       // test();
+       // test();
+       // test();
+
+        //echo $x;
+
+        // echo & print
+
+       // echo("Hello!"), ("Hello!"), ("Hello!")  ;
+       // echo"World!", "World!" ;
+       // print("Hello!");
+       // print "World!";
+
+       // $x =  print("Hello!");
+        // echo $x;
+
+
+       // $a = "Flaw";
+       // $b = 5;
+       // $c = 4.1;
+       // $d = true;
+      //  $e = [1,2,3];
+       // $f = new stdClass();
+      // $g = null;
+       // $h = fopen("php_practice.php", "r");
+
+      //  var_dump($a);
+      //  echo "<br>";
+      //   var_dump($b);
+       // echo "<br>";
+       //  var_dump($c);
+       // echo "<br>";
+       //  var_dump($d);
+      //  echo "<br>";
+       //  var_dump($e);
+      //  echo "<br>";
+      //   var_dump($f);
+       // echo "<br>";
+      //   var_dump($g);
+      //  echo "<br>";
+       //  var_dump($h);
+       // echo "<br>";
+
+
+     //   echo strlen($a);
+
+      //  $x = "Allan Joshua";
+      //  echo str_replace("Allan", "aj" $x);
+
+     //   echo trim("Hello
+     //   World!              ");
+
+      //  var_dump(explode("u"));
+
+      //echo "Hello World";
+     // $x = "Hello";
+      //$y = "Love";
+      
+      
+     // echo $x . " " . $y,"<br>";
+      //echo "$x $y <br>" ;
+      //echo "Juan \$Immortal\" Enrile";
+
+        //casting
+       // $a = (bool) 'Jerry';
+       // $b = (bool) 5;
+       // $c = (bool) 4.5;
+       // $d = (bool) true;
+        //$g = (bool) null;
+       // $h = (bool) fopen("php_practice.php", "r");
+
+        // var_dump($a);
+        // echo "<br>";
+        // var_dump($b);
+        // echo "<br>";
+        //var_dump($c);
+        //echo "<br>";
+        //var_dump($d);
+        //echo "<br>";
+        //  //var_dump($g);
+        // echo "<br>";
+        //  var_dump($h);
+        // echo "<br>";
+      
+
+    //$x = pi();
+    // echo $x;
+
+    //echo abs(-5);
+    //echo sqrt(49);
+   // echo round(5.5);
+  // echo rand ();
+  
+
+  //constant
+
+  // $_asd = 5;
+// define("HAKDOG","HI");
+  //echo HAKDOG;
+
+  //function test(){
+    //echo HAKDOG;
+ // }
+ 
+//  test();
+    
+    //$x = 5;
+
+    //$x = $x + 10;
+    //$x += 10;
+
+    //echo $x;
+        //
+        // 
+        // $x % = 5;
+
+         //comparison
+       // var_dump(5 == (int) "asdasdas5asadasd"); //TRUE
+     //   var_dump(5 === 5);
+       // var_dump(5 <> 7);
+       // var_dump(5 !== "5");
+        
+       // echo 65 <=> 25;
+
+       // pre-incre / decre
+
+       //$d = 5;
+       // echo ++$d;
+      
+        // post-incre
+       //$e = 5;
+       // echo  $e++;
+       // echo $e;
+
+        //logical
+
+        //and &&
+       // var_dump(true and true ); //true
+        //var_dump(5== "5" && 5===5 );
+
+        //or
+       // var_dump(false or false ); //atleast one true
+
+        //xor
+        //var_dump(true xor false ); //only true
+
+        //not
+       // var_dump(!false );
+    
+       //.
+       $d = "Helo";
+
+        //.= append
+         
+            $d .= "love!"
+            echo $d;
+
+
+
+
+
+        
+
+    ?>
+</body>
+</html>
+
+
